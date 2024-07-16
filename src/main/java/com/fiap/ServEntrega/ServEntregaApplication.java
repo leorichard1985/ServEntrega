ca75f102bd3ebc7c700accbd3304d96e3b77f620
Leandro Conceição <leandro.vs2010@hotmail.com>
2024-07-15 21:31:15 -0300

    estrutura do projeto

---
+++ src/main/java/com/fiap/ServEntrega/ServEntregaApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.ServEntrega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServEntregaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServEntregaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/ServEntrega/ServEntregaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.ServEntrega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServEntregaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
